107407ef04598c3905a67c2f2bcdfaed445cc56f
Nutrition Platform Developer <developer@nutrition-platform.com>
2025-10-07 22:00:13 -0700

    Clear Metro cache on build

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 export EXPO_PUBLIC_SUPABASE_URL=https://seybhemcarmdinrvazmn.supabase.co
 export EXPO_PUBLIC_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNleWJoZW1jYXJtZGlucnZhem1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2MDUxMzksImV4cCI6MjA3MjE4MTEzOX0._JYrwr4bfXXz4XsYoZX6yuBfCo1Xvx9rWmMHaPt1lI8
 export EXPO_PUBLIC_DEV_MODE=false
-npx expo export --platform web
+npx expo export --platform web --clear
